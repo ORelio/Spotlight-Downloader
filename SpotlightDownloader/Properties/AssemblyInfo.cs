19f82dd86dd68fb0a3855f3c46a9bc72d7c46b4e
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-06-02 18:17:06 +0700

    remove duplicate assembly attributes

--- SpotlightDownloader/Properties/AssemblyInfo.cs
+++ SpotlightDownloader/Properties/AssemblyInfo.cs
@@ -1,15 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpotlightDownloader")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Spotlight Downloader")]
 [assembly: AssemblyCopyright("Copyright © ORelio 2018-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93b09cec-d6f7-4369-b1a5-12d6237395c1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
